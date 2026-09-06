384e6607d8c119ccac73e86906eb8c81bfd84f2f
raffanfatih <raffan.fatih@gmail.com>
2026-09-06 20:33:16 +0700

    Update GenreBuku.java
    tambah sedikit fitur

--- TugasP2/src/GenreBuku.java
+++ TugasP2/src/GenreBuku.java
@@ -1,48 +1,23 @@
-/**
- * Class GenreBuku (BookGenre)
- * Merepresentasikan kategori/genre dari buku
- */
 public class GenreBuku {
     private int idGenre;
     private String namaGenre;
     private String deskripsi;
 
-    // Constructor
     public GenreBuku(int idGenre, String namaGenre, String deskripsi) {
         this.idGenre = idGenre;
         this.namaGenre = namaGenre;
         this.deskripsi = deskripsi;
     }
 
-    // Getter Methods
-    public int getIdGenre() {
-        return idGenre;
-    }
+    public int getIdGenre() { return idGenre; }
+    public String getNamaGenre() { return namaGenre; }
+    public String getDeskripsi() { return deskripsi; }
 
-    public String getNamaGenre() {
-        return namaGenre;
-    }
+    public void setNamaGenre(String namaGenre) { this.namaGenre = namaGenre; }
+    public void setDeskripsi(String deskripsi) { this.deskripsi = deskripsi; }
 
-    public String getDeskripsi() {
-        return deskripsi;
-    }
-
-    // Setter Methods
-    public void setNamaGenre(String namaGenre) {
-        this.namaGenre = namaGenre;
-    }
-
-    public void setDeskripsi(String deskripsi) {
-        this.deskripsi = deskripsi;
-    }
-
-    // toString method
     @Override
     public String toString() {
-        return "GenreBuku{" +
-                "idGenre=" + idGenre +
-                ", namaGenre='" + namaGenre + '\'' +
-                ", deskripsi='" + deskripsi + '\'' +
-                '}';
+        return "Genre: " + namaGenre + " (" + deskripsi + ")";
     }
 }
