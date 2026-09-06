9b0a6109ac2b4d840ccb1354653dbf1c92d5d63d
raffanfatih <raffan.fatih@gmail.com>
2026-09-06 20:32:54 +0700

    Update Buku.java
    menyingkat beberapa bagian

--- TugasP2/src/Buku.java
+++ TugasP2/src/Buku.java
@@ -1,8 +1,3 @@
-/**
- * Class Buku (Book)
- * Merepresentasikan data buku di perpustakaan
- * Memiliki relasi dengan GenreBuku (Many-to-One)
- */
 public class Buku {
     private int idBuku;
     private String judul;
@@ -11,7 +6,6 @@ public class Buku {
     private int stok;
     private GenreBuku genre;
 
-    // Constructor
     public Buku(int idBuku, String judul, String pengarang, int tahunTerbit, int stok, GenreBuku genre) {
         this.idBuku = idBuku;
         this.judul = judul;
@@ -21,44 +15,16 @@ public class Buku {
         this.genre = genre;
     }
 
-    // Getter Methods
-    public int getIdBuku() {
-        return idBuku;
-    }
+    public int getIdBuku() { return idBuku; }
+    public String getJudul() { return judul; }
+    public String getPengarang() { return pengarang; }
+    public int getTahunTerbit() { return tahunTerbit; }
+    public int getStok() { return stok; }
+    public GenreBuku getGenre() { return genre; }
 
-    public String getJudul() {
-        return judul;
-    }
+    public void setStok(int stok) { this.stok = stok; }
+    public void setGenre(GenreBuku genre) { this.genre = genre; }
 
-    public String getPengarang() {
-        return pengarang;
-    }
-
-    public int getTahunTerbit() {
-        return tahunTerbit;
-    }
-
-    public int getStok() {
-        return stok;
-    }
-
-    public GenreBuku getGenre() {
-        return genre;
-    }
-
-    // Setter Methods
-    public void setStok(int stok) {
-        this.stok = stok;
-    }
-
-    public void setGenre(GenreBuku genre) {
-        this.genre = genre;
-    }
-
-    /**
-     * Method untuk meminjam buku (mengurangi stok)
-     * @return true jika peminjaman berhasil, false jika stok habis
-     */
     public boolean pinjamBuku() {
         if (stok > 0) {
             stok--;
@@ -67,23 +33,13 @@ public class Buku {
         return false;
     }
 
-    /**
-     * Method untuk mengembalikan buku (menambah stok)
-     */
     public void kembaliBuku() {
         stok++;
     }
 
-    // toString method
     @Override
     public String toString() {
-        return "Buku{" +
-                "idBuku=" + idBuku +
-                ", judul='" + judul + '\'' +
-                ", pengarang='" + pengarang + '\'' +
-                ", tahunTerbit=" + tahunTerbit +
-                ", stok=" + stok +
-                ", genre=" + genre.getNamaGenre() +
-                '}';
+        return "Buku [ID=" + idBuku + "] " + judul + " - " + pengarang + 
+               " | Stok: " + stok + " | " + genre.getNamaGenre();
     }
 }
